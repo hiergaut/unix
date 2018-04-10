76f2eed7bbff3b5d1b33ba755cc63ebd1a055269
gauthier <gauthierbouyjou@aol.com>
2018-04-10 15:11:06 +0200

    install pi

--- debian/forceIp.bash
+++ debian/forceIp.bash
@@ -1,8 +1,8 @@
 #! /bin/bash -e
 
 
-if [ -z $DISPLAY ]; then
+if [ -z $SSH_TTY ]; then
 
-    ip addr add 192.168.0.100/24 broadcast 192.168.0.255 dev eth0
-    ip route add default via 192.168.0.254
+    sudo ip addr add 192.168.0.100/24 broadcast 192.168.0.255 dev eth0
+    sudo ip route add default via 192.168.0.254
 fi
