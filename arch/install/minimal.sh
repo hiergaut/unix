1e457ca738c2054fa9a9854aa5186c3cf169ba32
gauthier <gauthierbouyjou@aol.com>
2020-04-13 18:25:44 +0200

    update

--- arch/install/minimal.sh
+++ arch/install/minimal.sh
@@ -226,7 +226,7 @@ echo "$timezone" > $temp/timeZone
 	# device=$($DIALOG --backtitle "$appTitle" --title "Select device to install" --menu "$(echo && lsblk -S -e 11 && echo)" 0 0 0 \
 	# device=$($DIALOG --backtitle "$appTitle" --title "Select device to install" --menu "$(echo && lsblk -d && echo)" 0 0 0 \
 	# device=$($DIALOG --backtitle "$appTitle" --title "Select device to install" --menu "$(echo && lsblk -S && echo)" 0 80 0 "${options[@]}" 3>&1 1>&2 2>&3)
-	device=$($DIALOG --backtitle "$appTitle" --title "Select device to install" --menu "$(echo && lsblk && echo)" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
+	device=$($DIALOG --backtitle "$appTitle" --title "Select device to install" --menu "$(echo && lsblk -f && echo)" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
 
 	if [ $device = "nvme0n1" ]; then
 		post="p"
